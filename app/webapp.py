eca43da0733cf78a9567abf165687ec2545dbab3
Daniel Teycheney <danielfjteycheney@gmail.com>
2020-05-22 10:56:09 +1000

    Adding extra space so that pylama passes check.

--- app/webapp.py
+++ app/webapp.py
@@ -16,6 +16,7 @@ app = connexion.App(__name__, specification_dir="./")
 # Read the swagger.yml file to configure the endpoints
 app.add_api("swagger.yml")
 
+
 # Create a route to the base "/" page
 @app.route("/")
 def index():
